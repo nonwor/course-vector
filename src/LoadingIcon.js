bccd3e8302aff97fb3f1bd696d67da5ea27675f3
nonwor <nonwor@gmail.com>
2023-05-30 12:04:15 -0700

    loading icon

---
+++ src/LoadingIcon.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Loading.css';
+
+const LoadingIcon = () => {
+  return <div className="loading-icon">*</div>;
+};
+
+export default LoadingIcon;
--- src/userInputComponent/input.js
+++ src/userInputComponent/input.js
@@ -3,6 +3,7 @@ import {useState, useEffect} from "react";
 import Result from "../resultComponent/result";
 import './input.css';
 import { useNavigate } from 'react-router-dom';
+import LoadingIcon from '../LoadingIcon.js';
 
 import axios from 'axios';
 
@@ -10,6 +11,7 @@ const UserInput =()=>{
 
     const[userInput, setUserInput] = useState('');
     const[data, setData] = useState({});
+    const[loading, setLoading] = useState(false);
 
     useEffect(() => {
        
@@ -29,12 +31,14 @@ const UserInput =()=>{
     const captureAndSend=(e)=>{
         e.preventDefault();
         console.log(userInput);
+        setLoading(true);
 
         const fetchData = async () => {
             try {
                 const response = await axios.get(`http://127.0.0.1:8000/${userInput}`);
                 setData(response.data);
                 console.log("Done from Server!");
+                setLoading(false)
                 
             } catch (error) {
               console.error(error);
@@ -54,12 +58,13 @@ const UserInput =()=>{
             <h3>Online Course Recommender using Word2Vec</h3>
             <div className="info-search">
                 <div className="academicSupport">
+                    
                     Summary and Reference:
-                    <p>The goal of this project is to use word embeddings, to find courses related to job title.</p>
+                    <p className="paragraphSupport">This project utilize word embeddings to discover courses relevant to job titles or areas of interest. Word embeddings offer flexible context-based searching, eliminating the need for lexicons or word matching.</p>
                     <a href="/pdf/team143report.pdf" target="_blank" rel="noopener noreferrer">
                         Academic Paper
                     </a>
-                    <a href="/pdf/team143poster.pdf" target="_blank" rel="noopener noreferrer">
+                    <a className="poster" href="/pdf/team143poster.pdf" target="_blank" rel="noopener noreferrer">
                         Poster Presentation
                     </a>
                 </div>
@@ -67,12 +72,6 @@ const UserInput =()=>{
                 <form className="inputform" onSubmit={captureAndSend}>
                     <label>
                         Career/Area of Interest: 
-                        {/* <input
-                        type="text"
-                        name="career"
-                        value={userInput}
-                        onChange={handleChange}
-                        /> */}
                     </label>
                     <input
                         type="text"
@@ -80,12 +79,14 @@ const UserInput =()=>{
                         value={userInput}
                         onChange={handleChange}
                         />
-                    <button type="submit">Search</button>
+                    <button type="submit">
+                        {loading? <LoadingIcon />: 'Search'}
+                    </button>
                     <button onClick={handleButtonClick}>Go to Saved</button>
                 </form>
 
                 <div className="about">
-                    Links
+                    About:
                 </div>
                 
             </div>

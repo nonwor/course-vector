47f40039461cb705cbc965a89b0c900a8915aa49
nonwor <nonwor@gmail.com>
2023-05-30 10:23:52 -0700

    done

--- src/savedPage/saved.js
+++ src/savedPage/saved.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import './save.css'
 
 
 const Saved = () =>{
@@ -35,25 +36,35 @@ const Saved = () =>{
     }
 
     return(
-        <div>
-            Saved Item
-            <button onClick={handleButtonClick}>Go to Home</button>
-            {selectedItems.map(item => (
-                    <div key={item.cos_sim} className="courseItem">
-                        
-                        {/* We try to populat items if it's already been checked */}
-                        <input
-                        type="checkbox"
-                        checked={item}
-                        onChange={event => handleCheckboxChange(event, item)}
-                        />
-                        <p className="title">{item.orig_title}</p>
-                        <p className="source">Source: {item.source}</p>
-                        <p className="rating">Rating: {item.rating||"N/A"}</p>
-                        <p className="hours">Length: {item.hours||"N/A"} hours</p>
-                        <a href={`https://www.google.com/search?q=${item.search}+${item.source}`} target="_blank" className="buttonlink">Go to Course</a>  
-                    </div>
-                ))}
+        <div className="page">
+
+            <div className="mainnav">                
+                <h3 className="pagetitle">Saved Courses</h3>
+                <button className="backtohome" onClick={handleButtonClick}>Go to Home</button>
+            </div>
+            <div className="scroll-container">
+                {selectedItems.map(item => (
+                        <div key={item.cos_sim} className="courseItem">
+                            
+                            {/* We try to populat items if it's already been checked */}
+                            <input
+                            className="checkbox"
+                            type="checkbox"
+                            checked={item}
+                            onChange={event => handleCheckboxChange(event, item)}
+                            />
+                            <p className="title" 
+                                title={item.description}>
+                                {item.orig_title}
+                            </p>
+                            <p className="source">Source: {item.source}</p>
+                            <p className="rating">Rating: {item.rating||"N/A"}</p>
+                            <p className="hours">Length: {item.hours||"N/A"} hours</p>
+                            <a href={`https://www.google.com/search?q=${item.search}+${item.source}`} target="_blank" className="buttonlink">Go to Course</a>  
+                        </div>
+                    ))}
+            </div>
+            
             <button onClick={saveEdit}>Save Edit</button>
         </div>
     )
